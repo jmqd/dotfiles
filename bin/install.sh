41394f88132e9917ce1b3d1b351725983b0a54b5
Jordan McQueen <jordanmq@amazon.com>
2018-07-05 03:28:04 -0700

    symbolic links to sync'd cloudhome

--- bin/install.sh
+++ bin/install.sh
@@ -11,38 +11,47 @@ aws (w/ credentials configured)
 EOF
 
 # give myself a chance to ctrl-c if I realize I'm missing a dependency
-sleep 5s
+sleep 3s
 
 # ******************************************************************************
 # Create directories
 # ******************************************************************************
+echo "Creating common directories in ~/ ..."
 mkdir -p ~/src
 mkdir -p ~/cloud
 mkdir -p ~/cloud/mcqueen.jordan
+mkdir -p ~/.aws
 
 # ******************************************************************************
 # Copy cloud data
 # ******************************************************************************
+echo "Copying data from your cloud bucket..."
 aws s3 sync s3://mcqueen.jordan ~/cloud/mcqueen.jordan/
 
 # ******************************************************************************
-# Copy dotfiles not in source control
+# Symbolically link dotfiles not in source control
+# TODO: Make this recursively ln -s all of them. It's a tad bit tricky...
 # ******************************************************************************
-cp -a ~/cloud/mcqueen.jordan/dotfiles/. ~
+echo "Symbolically linking dotfiles not in source control..."
+ln -s ~/cloud/mcqueen.jordan/dotfiles/.aws/credentials ~/.aws/credentials
+ln -s ~/cloud/mcqueen.jordan/dotfiles/.env ~/.env
 
 # ******************************************************************************
 # Source non-public environment variables
 # ******************************************************************************
+echo "Sourcing environment variables..."
 . ~/.env
 
 # ******************************************************************************
 # Clone repos
 # ******************************************************************************
+echo "Cloning git repos..."
 git clone https://github.com/mcqueenjordan/cloudhome.git ~/src/cloudhome
 
 # ******************************************************************************
 # setup cron stuff
 # ******************************************************************************
+echo "Setting up crontab stuff..."
 echo "* * * * * ~/src/cloudhome/cloudhome/cloudhome.py" >> /tmp/cronstate
 crontab /tmp/cronstate
 rm /tmp/cronstate
