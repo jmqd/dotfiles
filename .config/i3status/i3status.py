605a6b11a6431893e7d2d472fc54628cdb33988d
Jordan McQueen <jordan@whoami.sh>
2019-02-05 15:45:29 -0800

    add i3status.py configuration

---
+++ .config/i3status/i3status.py
@@ -0,0 +1,111 @@
+from i3pystatus import Status
+from i3pystatus.calendar import google
+from i3pystatus.core import color
+import os
+
+color.ColorRangeModule.start_color = '#000000'
+
+status = Status()
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+    format="%Y-%m-%d %X",)
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+    format="{temp:.0f}°C",)
+
+# The battery monitor has many formatting options, see README for details
+
+# This would look like this, when discharging (or charging)
+# ↓14.22W 56.15% [77.81%] 2h:41m
+# And like this if full:
+# =14.22W 100.0% [91.21%]
+#
+# This would also display a desktop notification (via D-Bus) if the percentage
+# goes below 5 percent while discharging. The block will also color RED.
+# If you don't have a desktop notification demon yet, take a look at dunst:
+# http://www.knopwob.org/dunst/
+status.register("battery",
+    format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+    alert=True,
+    alert_percentage=5,
+    not_present_text="",
+    status={
+        "DIS": "↓",
+        "CHR": "↑",
+        "FULL": "=",
+    },)
+
+# This would look like this:
+# Discharging 6h:51m
+status.register("battery",
+    format="{status} {remaining:%E%hh:%Mm}",
+    alert=True,
+    alert_percentage=5,
+    not_present_text="",
+    status={
+        "DIS":  "Discharging",
+        "CHR":  "Charging",
+        "FULL": "Bat full",
+    },)
+
+# Displays whether a DHCP client is running
+status.register("runwatch",
+    name="DHCP",
+    path="/var/run/dhclient*.pid",)
+
+# Shows the address and up/down state of eth0. If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (eth0) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces
+status.register("network",
+    interface="enp0s31f6",
+    on_leftclick="ip addr show dev {interface} | xmessage -file -",
+    format_up = "\[{interface}\]\[{v4cidr}\]: {bytes_recv:0>3}KiB {bytes_sent:0>3}KiB",
+    format_down = "",
+)
+
+# Note: requires both netifaces and basiciw (for essid and quality)
+status.register("network",
+    interface="wlan0",
+    format_up="{essid} {quality:03.0f}%",)
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+status.register("disk",
+    path="/",
+    format="{used}/{total}G",)
+
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+status.register("pulseaudio",
+    format="♪{volume}",)
+
+# Shows mpd status
+# Format:
+# Cloud connected▶Reroute to Remain
+status.register("spotify")
+
+status.register("calendar",
+                format="\[{status}\] {title} {remaining}",
+                update_interval=10,
+                on_leftclick='xdg-open http://c/',
+                urgent_blink=True,
+                backend=google.Google(
+                    credential_path=os.path.expanduser("~/token.pickle"),
+                    credentials_json=os.path.expanduser("~/secret/client_id.json"))
+)
+
+status.run()
