fce864d0cf3b338b80088ff3af656cb5b1320b56
Jordan McQueen <j@jm.dev>
2023-07-09 10:40:34 +0000

    Update config.el name and font

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Jordan Logan McQueen"
+(setq user-full-name "Jordan McQueen"
       user-mail-address "j@jm.dev")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
@@ -21,7 +21,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "JetBrains Mono" :size 16))
+(setq doom-font (font-spec :family "monospace" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
