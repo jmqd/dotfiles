0c715833014d767032b4897f4fc578f5caf52412
Jordan McQueen <j@jm.dev>
2026-03-25 10:09:19 +0900

    Migrate Linux-specific Home Manager settings

--- home/linux.nix
+++ home/linux.nix
@@ -1,6 +1,60 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   imports = [ ./common.nix ];
 
-  # Linux-specific Home Manager modules and packages will be added here.
+  nixpkgs.config.allowUnfree = true;
+
+  home.sessionVariables = {
+    TERMINAL = "wezterm";
+    VISUAL = "emacsclient -c";
+  };
+
+  home.shellAliases = {
+    top = "btm";
+  };
+
+  home.packages =
+    (with pkgs; [
+      awscli2
+      bottom
+      flameshot
+      imagemagick
+      killall
+      nixfmt
+      pandoc
+      pmutils
+      shellcheck
+      shfmt
+      unzip
+      zip
+    ])
+    ++ lib.optionals pkgs.stdenv.hostPlatform.isx86_64 (with pkgs; [
+      discord
+      lutris
+      slack
+      spotify
+    ]);
+
+  programs = {
+    chromium.enable = true;
+    google-chrome.enable = pkgs.stdenv.hostPlatform.isx86_64;
+    rofi.enable = true;
+
+    nix-index = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    emacs = {
+      enable = true;
+      package = pkgs.emacs;
+    };
+
+    i3status-rust.enable = true;
+  };
+
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacs;
+  };
 }
