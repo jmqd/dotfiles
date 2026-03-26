34268f3bc7cdb417ff92533a14ae6cfa5d405725
Jordan McQueen <j@jm.dev>
2026-03-26 14:21:58 +0900

    Enable cgo for direnv on Darwin

--- home/direnv.nix
+++ home/direnv.nix
@@ -1,7 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  direnvPackage =
+    if pkgs.stdenv.hostPlatform.isDarwin then
+      pkgs.direnv.overrideAttrs (old: {
+        env = (old.env or { }) // {
+          CGO_ENABLED = 1;
+        };
+      })
+    else
+      pkgs.direnv;
+in
 {
   programs.direnv = {
     enable = true;
+    package = direnvPackage;
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
