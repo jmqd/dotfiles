c23ea87bb68be115ffe5a1192763cd42c00313cd
Jordan McQueen <j@jm.dev>
2026-04-14 02:20:11 +0900

    emacs: isolate native-comp cache in managed state

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,11 +1,18 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
+(let* ((var-dir (expand-file-name "var/" user-emacs-directory))
+       (eln-cache-dir (expand-file-name "eln-cache/" var-dir)))
+  (make-directory eln-cache-dir t)
+  (when (fboundp 'startup-redirect-eln-cache)
+    (startup-redirect-eln-cache eln-cache-dir)))
+
 (setq package-enable-at-startup nil
       inhibit-startup-message t
       inhibit-startup-screen t
       initial-scratch-message nil
       frame-inhibit-implied-resize t
-      native-comp-async-report-warnings-errors 'silent)
+      native-comp-async-report-warnings-errors 'silent
+      native-comp-warning-on-missing-source nil)
 
 (menu-bar-mode -1)
 
