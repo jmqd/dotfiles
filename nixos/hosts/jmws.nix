cd97ae862e2dba59b7154f12bcb989c9762a145a
Jordan McQueen <j@jm.dev>
2026-08-25 01:14:23 -0700

    Enable doas through the NixOS module

--- nixos/hosts/jmws.nix
+++ nixos/hosts/jmws.nix
@@ -193,7 +193,6 @@ in
   environment.systemPackages =
     (with pkgs; [
       # system admin / linux core
-      doas
       lshw
       pciutils
       vim
@@ -285,6 +284,7 @@ in
   programs.dconf.enable = true;
   programs.zsh.enable = true;
   programs.ydotool.enable = true;
+  security.doas.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
