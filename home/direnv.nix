808e14485595110190bae399d8b7a71b63d1594e
Jordan McQueen <j@jm.dev>
2026-09-05 12:39:49 -0700

    Reuse the cached Darwin direnv package

--- home/direnv.nix
+++ home/direnv.nix
@@ -1,22 +1,8 @@
 { pkgs, ... }:
-let
-  direnvPackage =
-    if pkgs.stdenv.hostPlatform.isDarwin then
-      pkgs.direnv.overrideAttrs (old: {
-        # The upstream direnv test suite can hang on Darwin in Nix builds
-        # (observed stuck in `make test-go test-bash test-fish test-zsh`).
-        doCheck = false;
-        env = (old.env or { }) // {
-          CGO_ENABLED = 1;
-        };
-      })
-    else
-      pkgs.direnv;
-in
 {
   programs.direnv = {
     enable = true;
-    package = direnvPackage;
+    package = pkgs.direnv;
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
