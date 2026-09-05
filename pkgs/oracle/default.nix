3b405ce690c3b2a02b8076704332633a00e0ed79
Jordan McQueen <j@jm.dev>
2026-09-05 10:10:13 -0700

    Update Oracle dependencies

--- pkgs/oracle/default.nix
+++ pkgs/oracle/default.nix
@@ -7,7 +7,7 @@ buildNpmPackage rec {
   version = "0.18.0";
 
   src = ./.;
-  npmDepsHash = "sha256-65r2jLEMCaaPbf/kVHolhqTWQ2w7y4o2fA2hGClqq3s=";
+  npmDepsHash = "sha256-ZcTODXuzG+SvmfHAxAmyyLW3T1b2U6hhakmUBxCrkCI=";
   npmDepsFetcherVersion = 2;
   dontNpmBuild = true;
 
