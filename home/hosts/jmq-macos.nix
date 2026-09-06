bbd34c01bbf5d1f08c3e94af4f5b314b25d8d894
Jordan McQueen <j@jm.dev>
2026-09-05 22:56:02 -0700

    Fix OMP phone Tailscale hostname

--- home/hosts/jmq-macos.nix
+++ home/hosts/jmq-macos.nix
@@ -6,7 +6,7 @@
   home.homeDirectory = "/Users/jmq";
 
   services.omp-phone = lib.mkIf pkgs.stdenv.hostPlatform.isAarch64 {
-    publicUrl = "https://jordans-macbook-pro.taild6d9b.ts.net/omp/";
+    publicUrl = "https://jordans-macbook-pro-1.taild6d9b.ts.net/omp/";
     allowedTailnetUsers = [ "j@jm.dev" ];
     tailnetCapability = "jm.dev/cap/omp-phone";
   };
